595302ad514c8c720ce6ee2592713b7f4876238e
shaury srivastav <60475967+shaurysrivastav@users.noreply.github.com>
2020-05-29 13:52:43 +0530

    CRICKET SCORESHEET

---
+++ welcome.h
@@ -0,0 +1,29 @@
+void welcome()
+{
+                int i,y;
+                locate(26,5);
+                locate(27,7);
+                  locate(27,9);
+            locate(27,11);
+                  printf(" !!! YOU ARE WELCOME !!!");
+                  locate(39,13);
+                  printf("TO");
+                  locate(27,15);
+                  printf("  Cricket score sheet ");
+                  locate(27,17);
+                  Sleep(250);
+                  for(y=35;y<=58;y++)
+                  {
+                    {
+
+                      locate(27,y);
+                      Sleep(400);
+                    }
+                  }
+                  printf("\n");
+            }
+
+
+
+
+
